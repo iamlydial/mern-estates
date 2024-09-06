318fcdc7fdfbc28493e75d798441a519eb88efa4
iamlydial <lydia.lavecchia@gmail.com>
2024-09-06 21:34:56 +0200

    routing
    - add react-router-dom
    - add routes: Home, About, Profile, Sign in, Sign out

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-3xl text-ellipsis font-bold underline">Hello world!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
