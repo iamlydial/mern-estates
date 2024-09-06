0e965e30c419ddea4875344e0e0b30f29a3e82d0
iamlydial <lydia.lavecchia@gmail.com>
2024-09-06 21:50:08 +0200

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <span className="text-slate-500">Mern</span>
+          <span className="text-slate-700">Estate</span>
+        </h1>
+        <form className="bg-slate-100 p-3 rounded-lg">
+          <input className="bg-transparent" type="text" placeholder="Search..." />
+        </form>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
