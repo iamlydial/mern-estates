764affa7609cf031f2a51f861778dc9a8288b724
iamlydial <lydia.lavecchia@gmail.com>
2024-09-11 16:08:34 +0200

    add Signup Page markup

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input type="text" id="username" placeholder="username" className="border p-3 rounded-lg" />
+        <input type="email" id="email" placeholder="email" className="border p-3 rounded-lg" />
+        <input type="password" id="password" placeholder="password" className="border p-3 rounded-lg" />
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
