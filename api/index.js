4fff6443cfa687dbf89d9a9e4f39b6cfe736951a
iamlydial <lydia.lavecchia@gmail.com>
2024-09-17 22:02:29 +0200

    create update api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 mongoose
@@ -18,6 +19,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
@@ -25,12 +28,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'internal server error';
+  const message = err.message || "internal server error";
   return res.status(statusCode).json({
     success: false,
     statusCode: statusCode,
-    message
-  })
-})
+    message,
+  });
+});
