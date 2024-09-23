b887098e0ef69ab64318baaa1edc4940b609e00c
iamlydial <lydia.lavecchia@gmail.com>
2024-09-23 19:09:50 +0200

    Update CreateListing.jsx
    complete cerate listing page UI

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,7 +1,156 @@
 import React from "react";
 
 const CreateListing = () => {
-  return <div className="mt-11">CreateListing</div>;
+  return (
+    <main className="p-3 max-w-4xl mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">
+        Create a Listing
+      </h1>
+      <form className="flex flex-col sm:flex-row gap-4">
+        <div className="flex flex-col gap-4 flex-1">
+          <input
+            className="border rounded-lg p-3"
+            type="text"
+            placeholder="name"
+            name="name"
+            id="name"
+            maxLength="62"
+            minLength="10"
+            required
+          />
+          <input
+            className="border rounded-lg p-3"
+            placeholder="description"
+            type="text"
+            name="description"
+            id="description"
+            required
+          />
+          <input
+            className="border rounded-lg p-3"
+            type="text"
+            placeholder="address"
+            name="address"
+            id="address"
+            required
+          />
+
+          <div className="flex gap-6 flex-wrap">
+            <div className="flex gap-2">
+              <input type="checkbox" name="sell" id="sell" className="w-5" />
+              <span>Sell</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" name="reny" id="rent" className="w-5" />
+              <span>Rent</span>
+            </div>
+            <div className="flex gap-2">
+              <input
+                type="checkbox"
+                name="parking"
+                id="parking"
+                className="w-5"
+              />
+              <span>Parking Spot</span>
+            </div>
+            <div className="flex gap-2">
+              <input
+                type="checkbox"
+                name="furnished"
+                id="furnished"
+                className="w-5"
+              />
+              <span>Furnished</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" name="offer" id="offer" className="w-5" />
+              <span>Offer</span>
+            </div>
+            <div className="flex flex-wrap gap-6">
+              <div className="flex items-center gap-2">
+                <input
+                  type="number"
+                  name=""
+                  id="bedrooms"
+                  min="1"
+                  max="10"
+                  required
+                  className="p-3 border border-gray-300 rounded-lg"
+                />
+                <p>Beds</p>
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  type="number"
+                  name=""
+                  id="bathrooms"
+                  min="1"
+                  max="10"
+                  required
+                  className="p-3 border border-gray-300 rounded-lg"
+                />
+                <p>Baths</p>
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  type="number"
+                  name="regularPrice"
+                  id="regularPrice"
+                  min="1"
+                  max="10"
+                  required
+                  className="p-3 border border-gray-300 rounded-lg"
+                />
+                <div className="flex flex-col items-center">
+                  <p>Regular Price</p>
+                  <span className="text-xs">($ / month)</span>
+                </div>
+              </div>
+              <div className="flex items-center gap-2">
+                <input
+                  type="number"
+                  name="discountedPrice"
+                  id="discountedPrice"
+                  min="1"
+                  max="10"
+                  required
+                  className="p-3 border border-gray-300 rounded-lg"
+                />
+                <div className="flex flex-col items-center">
+                  <p>Discounted Price</p>
+                  <span className="text-xs">($ / month)</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col flex-1 gap-4">
+          <p className="font-semibold">
+            Images:{" "}
+            <span className="text-gray-600 ml-2">
+              The first image will be the cover (max 6)
+            </span>
+          </p>
+          <div className="flex gap-4">
+            <input
+              className="p-3 border border-gray-300 rounded w-full"
+              type="file"
+              name=""
+              id="images"
+              accept="image/*"
+              multiple
+            />
+            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
+              Upload
+            </button>
+          </div>
+          <button className="p-3 bg-slate-700 text-white uppercase rounded-lg hover:opacity-95 disabled:opacity-80">
+            Create Listing
+          </button>
+        </div>
+      </form>
+    </main>
+  );
 };
 
 export default CreateListing;
