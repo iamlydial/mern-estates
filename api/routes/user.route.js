e797c6e2e49ed82ba254802129fbaf495b55db81
iamlydial <lydia.lavecchia@gmail.com>
2024-09-25 11:09:36 +0200

    create get user listings api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { test, updateUser, deleteUser } from "../controllers/user.controller.js";
+import {
+  test,
+  updateUser,
+  deleteUser,
+  getUserListings,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const userRouter = express.Router();
@@ -7,5 +12,6 @@ const userRouter = express.Router();
 userRouter.get("/test", test);
 userRouter.post("/update/:id", verifyToken, updateUser);
 userRouter.delete("/delete/:id", verifyToken, deleteUser);
+userRouter.get("/listings/:id", verifyToken, getUserListings);
 
 export default userRouter;
