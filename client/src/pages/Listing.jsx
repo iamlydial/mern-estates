e4323bd9a91ee79ba71d2356bd222affca438728
iamlydial <lydia.lavecchia@gmail.com>
2024-09-26 17:02:36 +0200

    add swiper element for listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
 
 const Listing = () => {
+  SwiperCore.use([Navigation]);
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [error, setError] = useState(false);
@@ -29,7 +34,31 @@ const Listing = () => {
     };
     fetchListing();
   }, [params.listingId]);
-  return <div>Listing Page {listing?.name}</div>;
+  return (
+    <main>
+      {loading && <p className="text-center my-7 text-2xl ">Loading...</p>}
+      {error && (
+        <p className="text-center my-7 text-2xl ">Something went wrong!</p>
+      )}
+      {listing && !loading && !error && (
+        <div>
+          <Swiper navigation>
+            {listing.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className="h-[500px]"
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      )}
+    </main>
+  );
 };
 
 export default Listing;
