554c492633dc52085ae045f3fa0c9036bce2a2d2
iamlydial <lydia.lavecchia@gmail.com>
2024-09-27 18:00:05 +0200

    Update Contact.jsx
    contact landlord functionality to the listing page and contact page UI

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,8 +1,55 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Contact = ({ listing }) => {
-  return <div>Contact</div>;
+  const [landlord, setLandlord] = useState(null);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const fetchLandlord = async () => {
+      try {
+        const res = await fetch(`/api/user/${listing.userRef}`);
+        const data = await res.json();
+        setLandlord(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchLandlord();
+  }, [listing.userRef]);
+
+  const onChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  return (
+    <>
+      {landlord && (
+        <div className="flex flex-col gap-2">
+          <p>
+            Contact <span className="font-semibold ">{landlord.username}</span>{" "}
+            for{" "}
+            <span className="font-semibold">{listing.name.toLowerCase()}</span>
+          </p>
+          <textarea
+            name="message"
+            id="message"
+            rows="2"
+            value={message}
+            onChange={onChange}
+            className="w-full border p-3 rounded-lg"
+            placeholder="Enter your message here"
+          ></textarea>
+          <Link
+            to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+            className="bg-slate-700 rounded-lg text-white text-center p-3 uppercase hover:opacity-95"
+          >
+            Send Message
+          </Link>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Contact;
