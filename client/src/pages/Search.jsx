9443ed3156d34b6ce6cedb7d32e53a218732c06f
iamlydial <lydia.lavecchia@gmail.com>
2024-09-28 11:01:53 +0200

    Update Search.jsx
    style search column adding search type and amenities

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,7 +1,58 @@
 import React from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="flex flex-col md:flex-row">
+      <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
+        <form className="flex flex-col gap-8">
+          <div className="flex items-center gap-2">
+            <label className="whitespace-nowrap">Search Term:</label>
+            <input
+              type="text"
+              name=""
+              placeholder="Search..."
+              id="searchTerm"
+              className="border rounded-lg p-3 w-full"
+            />
+          </div>
+          <div className="flex gap-2 flex-wrap items-center">
+            <label>Type:</label>
+            <div className="flex gap-2">
+              <input type="checkbox" id="all" className="w-5" />
+              <span>Rent & Sale</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="rent" className="w-5" />
+              <span>Rent</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="sale" className="w-5" />
+              <span>Sale</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="offer" className="w-5" />
+              <span>Offer</span>
+            </div>
+          </div>
+
+          <div className="flex gap-2 flex-wrap items-center">
+            <label>Amenities:</label>
+            <div className="flex gap-2">
+              <input type="checkbox" id="parking" className="w-5" />
+              <span>Parking</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="furnished" className="w-5" />
+              <span>Furnished</span>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div>
+        <h1>Listing Results:</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
